f672f9cb47f92870c03195064f283ca579bd1909
Darío <dariogarcigarci@gmail.com>
2022-06-03 12:49:41 +0200

    Modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component'
-import { HeroeComponent } from './heroes/heroe/heroe.component';
-import { ListadoComponent } from './heroes/listado/listado.component';
-
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadoresModule } from './contadores/contadores.module';
 @NgModule({
   declarations: [
-    AppComponent,
-    ContadorComponent,
-    HeroeComponent,
-    ListadoComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadoresModule
   ],
   providers: [],
   bootstrap: [AppComponent]
