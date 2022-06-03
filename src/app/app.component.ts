19b09ba34152192498549fb910afb7ddc3901c77
Darío <dariogarcigarci@gmail.com>
2022-06-03 11:50:15 +0200

    Separado el contador a un component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public titulo: string = 'Contador app';
-  public numero: number = 0;
-  public base: number = 5;
 
-  acumular(valor: number): number{
-    return this.numero += valor;
-  }
 }
